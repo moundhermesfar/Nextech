48435fe764d5482146556f6977c08fe3ac813e22
moundher mesfar <mesfarmounethe@gmail.com>
2023-12-04 18:58:38 +0100

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { disableReactDevTools } from "@fvilers/disable-react-devtool";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 
 if (process.env.NODE_ENV === "production") disableReactDevTools();
 
