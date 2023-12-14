e1aca3a7c7b9768baaf91fc5e426d7b646317b63
moundher mesfar <mesfarmounether@gmail.com>
2023-12-14 08:32:44 +0100

    not last commit

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -6,29 +6,55 @@ import MembersTable from "../components/MembersTable";
 const AdminPage = () => {
   const [members, setMembers] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [password, setPassword] = useState("");
+  const [authenticated, setAuthenticated] = useState(false);
+
+  const handlePasswordSubmit = () => {
+    const correctPassword = "NexTech23";
+
+    if (password === correctPassword) {
+      setAuthenticated(true);
+    } else {
+      alert("Incorrect password.");
+    }
+  };
 
   useEffect(() => {
-    setLoading(true);
-    axios
-      .get("https://nextech-api.onrender.com/members")
-      .then((response) => {
-        setMembers(response.data.data);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.log(error);
-        setLoading(false);
-      });
-  }, []);
+    if (authenticated) {
+      setLoading(true);
+      axios
+        .get("https://nextech-api.onrender.com/members")
+        .then((response) => {
+          setMembers(response.data.data);
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.log(error);
+          setLoading(false);
+        });
+    }
+  }, [authenticated]);
 
   return (
     <div className="p-4">
       <div className="flex justify-between items-center">
         <h1 className="text-3xl my-8">Members List</h1>
+        {!authenticated && (
+          <div>
+            <input
+              type="password"
+              placeholder="Enter password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button onClick={handlePasswordSubmit}>Submit</button>
+          </div>
+        )}
       </div>
-      {loading ? <Spinner /> : <MembersTable members={members} />}
+      {loading ? <Spinner /> : authenticated && <MembersTable members={members} />}
     </div>
   );
 };
 
 export default AdminPage;
+
